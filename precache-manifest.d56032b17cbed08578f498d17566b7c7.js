498899beb88e6dfac5cd198cddd7c962747f3de6
Alysia Pereira <alysiapereira5@gmail.com>
2020-04-07 21:44:46 -0400

    Updates

---
+++ precache-manifest.d56032b17cbed08578f498d17566b7c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "279bd47cc004591dae546eeb2dd9ec00",
+    "url": "/user-directory/index.html"
+  },
+  {
+    "revision": "4b7e36cca7efe14fd6dd",
+    "url": "/user-directory/static/css/main.420fa115.chunk.css"
+  },
+  {
+    "revision": "9eb15337e6d7c1a37dc0",
+    "url": "/user-directory/static/js/2.0cabaaae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user-directory/static/js/2.0cabaaae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b7e36cca7efe14fd6dd",
+    "url": "/user-directory/static/js/main.419d092f.chunk.js"
+  },
+  {
+    "revision": "fe627a724b6a50218bf7",
+    "url": "/user-directory/static/js/runtime-main.b98e6c53.js"
+  }
+]);
